d2e8c322e9e02ffb34c65e439db880f6fddd0395
Lasse Carstensen <lasse.car@gmail.com>
2012-03-16 18:03:17 +0100

    A few more code examples

---
+++ stripper/test.cpp
@@ -0,0 +1,25 @@
+
+
+
+int main()
+    int test
+    bool test
+    if hej = test
+        thendostuff()
+        andmorestuff()
+
+    dothing()
+        
+    while hej != 0
+        hej()
+        for i:=1, i<10, i++
+            ding()
+
+            dong()
+
+            dang()
+            
+        hej()
+
+    hej()
+
